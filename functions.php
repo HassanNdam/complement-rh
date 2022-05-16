904a590d3865e3dfdc6a71f87bba146ba9d0df0a
Hassan Ndam <ndamfiverr@gmail.com>
2022-05-16 22:20:51 +0200

    ajout fonctions

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function complement_support(){
+    add_theme_support('title-tag'); 
+    add_theme_support('custom-logo');
+    add_theme_support("post-thumbnails");
+}
+
+add_action('after_setup_theme','complement_support');
+
+function complement_style(){
+
+    wp_enqueue_style( 'my-custom-style', get_template_directory_uri() . '/style.css', array('ms-bootstrap'), time() );
+    wp_enqueue_style('ms-bootstrap',"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css", array(), 
+    '5.1.3', 'All');
+    wp_enqueue_style('ms-font',"https://use.fontawesome.com/releases/v5.7.0/css/all.css", array(), 
+    '5.7.0', 'All');
+
+}
+add_action('wp_enqueue_scripts', 'complement_style');
+
+?>
