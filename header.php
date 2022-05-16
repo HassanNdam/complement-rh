60ab71bfcc7e3c5d127bc979790b93b22900d8ff
Hassan Ndam <ndamfiverr@gmail.com>
2022-05-16 22:23:58 +0200

    ajout favicone

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Complement-rh</title>
+    <link rel="icon" href="<?php echo (get_template_directory_uri() . '/assets/icone/icone.png') ?>" sizes="16x16 32x32 48x48 64x64">
     
 <?php
 wp_head();
