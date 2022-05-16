b9cb6926422e1aa0c55a25fa5ef4a9fc03c076cf
Hassan Ndam <ndamfiverr@gmail.com>
2022-05-17 00:19:30 +0200

    Mise en place header

--- header.php
+++ header.php
@@ -1,15 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Complement-rh</title>
-    <link rel="icon" href="<?php echo (get_template_directory_uri() . '/assets/icone/icone.png') ?>" sizes="16x16 32x32 48x48 64x64">
+    <title><?php echo bloginfo('name');?></title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="icon" href="<?php echo (get_template_directory_uri() . '/assets/icone/favicon.png') ?>" sizes="16x16 32x32 48x48 64x64">
     
 <?php
 wp_head();
 ?>
 </head>
 <body>
-    
+
+<header class="site-header bg-light shadow-sm position-sticky">
+    <nav class="navbar navbar-default navbar-expand d-flex container flex-md-row justify-content-between py-2">
+    <div class="container-fluid">
+        <a class="navbar-brand" title="Aller sur la page d'accueil" href="<?php echo get_site_url();?>">
+        <img src="<?php echo get_template_directory_uri(). '/assets/logo/logo.png'?>" alt="Logo Complement-RH" style="max-width:250px" class="img-fluid"> 
+        </a>
+    </div>
+    <div class="d-flex">
+        <a href="#" target="_blank">
+            <button type="button" class="btn btn-primary" onclick="this.blur();">Candidature spontanée</button>
+        </a>
+    </div>
+    </nav>
+
+</header>
+
+<div class="position-relative  p-3 p-md-5  overflow-hidden header-image">
+        <div class="text-center">
+                ddddddddd
+        </div>
+</div>
