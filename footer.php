0675c823bef42fd8fd5c468ccdfd6d5bd025b9fc
Hassan Ndam <ndamfiverr@gmail.com>
2022-05-17 01:20:44 +0200

    Realisation footer

--- footer.php
+++ footer.php
@@ -1,5 +1,89 @@
 
+<!-- Footer -->
+<footer class="text-center mt-5 text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+  <section
+    class="d-flex justify-content-center justify-content-lg-between text-white background-footer p-4 border-bottom"
+  >
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Suivez-nous :</span>
+    </div>
+    <!-- Left -->
 
+    <!-- Right -->
+    <div class="justify-content-lg-between">
+      <a href="https://www.linkedin.com/company/complement-rh/" class="me-4 text-reset" target="_blank">
+        <i class="fab fa-linkedin"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="pb-5">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4 titre-footer">
+            </i>© 2016 COMPLÉMENT RH - 
+          </h6>
+          <p>
+          SAS Complément RH SIRET 793 201 765 00039 
+          </p>
+          <a href="https://complement-rh.fr/" target="_blank">
+            <img class ="img-fluid mt-2"src="<?php echo (get_template_directory_uri() . '/assets/icone/favicon.png') ?>" alt="">
+          </a>
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4 titre-footer">
+            Important
+          </h6>
+          <p>
+            <a href="https://complement-rh.fr/mentions-legales/" class="text-reset" target="_blank">Mentions légales</a>
+          </p>
+          <p>
+            <a href="https://complement-rh.fr/politique-de-confidentialite/" class="text-reset" target="_blank">Politique de confidentialité</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4 titre-footer">
+            Contact
+          </h6>
+          <p><i class="fas fa-home me-3"></i> 
+                    3 rue des Cigognes – Aéroparc II  67960 ENTZHEIM
+            </p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            contact@complement-rh.fr
+          </p>
+          <p><i class="fas fa-phone me-3"></i> +33 6 71 78 18 21 </p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4 text-white" style="background-color: rgb(34, 34, 34);">
+    © 2021 Copyright @ Designed by:
+    <a class="text-reset fw-bold" href="https://paradisiak.com/" target="_blank">Paradisiak</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
