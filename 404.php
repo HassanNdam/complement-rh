f89cff3349e704fcb3007ed610875698713fc9f1
Hassan Ndam <ndamfiverr@gmail.com>
2022-05-17 11:36:45 +0200

    Ajout contenu page 404

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+get_header();
+?>
+<div class="container mt-5  text-center">
+    <p class="mt-5">
+        <span class="text-404">404 </span>
+        <h1>Oups! Aucune page ne correspond à votre demande</h1>
+    </p><br>
+    <a class="liste-offres mt-5" href="<?php echo get_site_url();?>">Revenir à la liste des offres </a>
+</div>
+
+<?php
+get_footer();
+?>
